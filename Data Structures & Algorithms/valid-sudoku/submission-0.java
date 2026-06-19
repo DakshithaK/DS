a0b618dbbb90465484b61591c00313834ea7895a
Dakshitha K <140424082+DakshithaK@users.noreply.github.com>
2026-06-19 22:28:40 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int row=0;row<9;row++){
+            Set<Character> present= new HashSet<>();
+            for(int i=0;i<9;i++){
+                if(board[row][i]=='.'){continue;}
+                if(present.contains(board[row][i])){
+                    return false;
+                }
+                present.add(board[row][i]);
+            }
+        }
+        for(int col=0;col<9;col++){
+            Set <Character> presenn=new HashSet<>();
+            for(int i=0;i<9;i++){
+                if(board[i][col]=='.'){continue;}
+                if(presenn.contains(board[i][col])){
+                    return false;
+                }
+                presenn.add(board[i][col]);
+            }
+        }
+        for(int sq=0;sq<9;sq++){
+            Set <Character> presenn=new HashSet<>();
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    int row= (sq/3)*3+i;
+                    int col=(sq%3)*3+j;
+                    if(board[row][col]=='.'){continue;}
+                if(presenn.contains(board[row][col])){
+                    return false;
+                }
+                presenn.add(board[row][col]);
+                }
+            }
+        }
+        return true;
+        
+    }
+}
